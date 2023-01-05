3ebdc5d963e2e0fb7705be578a8e2fe7a6ce117c
Stewart Lovell <lovell.stewart@student.greenriver.edu>
2023-01-05 17:13:52 -0500

    Add homepage with cupcake form

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cupcakes</title>
+</head>
+<body>
+    <h1>Cupcake Fundraiser</h1>
+    <form method="post" action="process.php">
+        <label><input type="text" name="name">Your name:</label>
+        <label for="name">Cupcake flavors:</label>
+        <label><input type="checkbox" name="flavor[]" value="The Grasshopper">The Grasshopper</label>
+        <label><input type="checkbox" name="flavor[]" value="Whiskey Maple Bacon">Whiskey Maple Bacon</label>
+        <label><input type="checkbox" name="flavor[]" value="Carrot Walnut">Carrot Walnut</label>
+        <label><input type="checkbox" name="flavor[]" value="Salted Caramel Cupcake">Salted Caramel Cupcake</label>
+        <label><input type="checkbox" name="flavor[]" value="Red Velvet">Red Velvet</label>
+        <label><input type="checkbox" name="flavor[]" value="Lemon Drop">Lemon Drop</label>
+        <label><input type="checkbox" name="flavor[]" value="Tiramisu">Tiramisu</label>
+        <input type="submit" value="Order">
+    </form>
+</body>
+</html>
